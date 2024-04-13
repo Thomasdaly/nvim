2cb1c56471e134e51f3b4c7ea9df8c50dfa74f3f
thomasdaly <tdaly91@gmail.com>
2024-04-14 01:32:23 +1000

    added copilot chat

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,4 +1,5 @@
-return {
+return{ 
+    {
 		"zbirenbaum/copilot.lua",
 		event = "VimEnter",
 		config = function()
@@ -8,4 +9,18 @@ return {
 				}
 			end, 100)
 		end,
-	}
+	},
+    {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+}
