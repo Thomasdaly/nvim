d01b8170bd6ce926e63d1709c3a5704ef91d3e34
thomasdaly <tdaly91@gmail.com>
2024-04-23 23:00:13 +1000

    fixed neorg

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,16 +1,32 @@
-   return{
-       {
-        "vhyrro/luarocks.nvim",
-        priority = 1000, -- We'd like this plugin to load first out of the rest
-        config = true, -- This automatically runs `require("luarocks-nvim").setup()`
-    },
-    {
-        "nvim-neorg/neorg",
-        dependencies = { "luarocks.nvim" },
-        -- put any other flags you wanted to pass to lazy here!
-        config = function()
-            require("neorg").setup({
-            })
-        end,
-    }}
-
+return {
+{
+    "vhyrro/luarocks.nvim",
+    priority = 1000, -- This plugin loads first out of the rest
+    config = function()
+        require("luarocks-nvim").setup({})  -- Setup luarocks within Neovim
+    end
+},
+{
+    "nvim-neorg/neorg",
+    dependencies = { "luarocks.nvim" }, -- Neorg depends on luarocks.nvim
+    config = function()
+        require("neorg").setup({
+            load = {
+                ["core.defaults"] = {}, -- Load all the default modules
+                ["core.concealer"] = {},
+                ["core.esupports.hop"] = {},
+                ["core.integrations.treesitter"] = {},
+                ["core.summary"]  = {},
+                ["core.dirman"] = { -- Manage Neorg directories
+                    config = {
+                        workspaces = {
+                            personal = "~/notes/personal", -- Path to your personal notes
+                            uni = "~/notes/uni" -- Path to your university notes
+                        }
+                    }
+                },
+            }
+        })
+    end,
+}
+}
