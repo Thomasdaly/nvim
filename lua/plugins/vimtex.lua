0066fe18f2cbb717e7d814feb62da483e5cd1d8f
thomadaly <tdaly91@gmail.com>
2024-04-23 17:05:27 +1000

    added neorg for note taking?

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -2,15 +2,15 @@ return {
   "lervag/vimtex", -- Plugin repository
   config = function() -- Configuration function for setting up the plugin after it's loaded
     -- Execute Vimscript commands using vim.cmd
-    vim.cmd [[
+    vim.cmd([[
       filetype plugin indent on
       syntax enable
       let g:vimtex_view_method = 'zathura'
-      let g:vimtex_view_general_viewer = 'okular'
+      let g:vimtex_view_general_viewer = 'zathura'
       let g:vimtex_view_general_options = '--unique file:@pdf#src:@line@tex'
-      let g:vimtex_compiler_method = 'latexrun'
+      let g:vimtex_compiler_method = 'latexmk'
+      let g:vimtex_mainfile_auto = 1
       let maplocalleader = ","
-    ]]
+    ]])
   end
 }
-
